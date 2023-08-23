ebe910d0c6339232cc0fbb658d29e4ae475ce9c9
Korchea <6334@holbertonstudents.com>
2023-08-23 09:38:34 -0700

    Task 4 circular import not solved

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Something to do later"""
+from api.v1.views.index import get_status
 from flask import Blueprint
-import api.v1.views.index
 
 app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
