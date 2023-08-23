9fbfe3194fc7b3795343a47d9f00fa5b0df497f5
Korchea <6334@holbertonstudents.com>
2023-08-23 10:17:31 -0700

    Task 4 doc added

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+""" Returns a JSON: 'status: OK'"""
 from api.v1.views import app_views
 from flask import jsonify
 
 
 @app_views.route('/status')
 def get_status():
+    """ Returns a JSON: 'status: OK"""
     return jsonify(status="OK")
