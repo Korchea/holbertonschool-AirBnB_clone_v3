9b73699114be9cd97ce65538435ef820c66dc1cf
Guillermo Vega <124533439+Korchea@users.noreply.github.com>
2023-08-23 13:33:56 -0300

    Update __init__.py, circular import solved

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Something to do later"""
-from api.v1.views.index import get_status
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
+
+from api.v1.views.index import get_status
