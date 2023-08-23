72d5851bfb1d32648e91b20b8350cffefc594e92
Korchea <6334@holbertonstudents.com>
2023-08-23 13:50:25 -0700

    Task 5 solved

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -17,6 +17,6 @@ def get_stats():
     return jsonify(amenities=storage.count("Amenity"),
                    cities=storage.count("City"),
                    places=storage.count("Place"),
-                   reviews=storage.count("Rewiew"),
+                   reviews=storage.count("Review"),
                    states=storage.count("State"),
                    users=storage.count("User"))
