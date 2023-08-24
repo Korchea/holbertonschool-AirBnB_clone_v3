671e3536e06add860b72c48179fe4f0917ba1150
Korchea <6334@holbertonstudents.com>
2023-08-23 17:48:28 -0700

    Task 7 completed

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -63,4 +63,4 @@ def update_state(state_id):
     for key, value in body_request:
         if key != 'id' and key != 'created_at' and key != 'updated_at':
             setattr(state, key, value)
-    return jsonify(state.to_dict())
+    return jsonify(state.to_dict()), 200
