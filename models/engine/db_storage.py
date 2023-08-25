ba9ce620a57dfa536bf2b75e4b7e843124e5d914
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-08-25 10:39:58 -0700

    fix given testing, version0

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -76,7 +76,8 @@ class DBStorage:
         self.__session.remove()
 
     def get(self, cls, id):
-        """ Returns the object based on the class and its ID, or None if not found"""
+        """ Returns the object based on the class and its ID,
+        or None if not found"""
         object = self.__session.query(cls)
         for obj in object:
             if obj.id == id:
